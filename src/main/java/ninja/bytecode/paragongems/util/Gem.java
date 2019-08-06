83298686717bba732b3b70467dfa0e90c5976076
Tim <dabon696@yahoo.com>
2019-08-05 22:05:49 -0400

    Implement block references

--- src/main/java/ninja/bytecode/paragongems/util/Gem.java
+++ src/main/java/ninja/bytecode/paragongems/util/Gem.java
@@ -1,5 +1,7 @@
 package ninja.bytecode.paragongems.util;
 
+import net.minecraft.item.ItemBlock;
+import ninja.bytecode.paragongems.base.BlockGemOre;
 import ninja.bytecode.paragongems.base.ItemGem;
 
 public class Gem implements IGem
@@ -7,6 +9,8 @@ public class Gem implements IGem
 	private final String id;
 	private final String name;
 	private ItemGem gem;
+	private BlockGemOre ore;
+	private ItemBlock ib;
 
 	public Gem(String id, String name)
 	{
@@ -37,5 +41,31 @@ public class Gem implements IGem
 	public ItemGem getGemItem()
 	{
 		return gem;
+	}
+
+	@Override
+	public void setGemOre(BlockGemOre block)
+	{
+		this.ore = block;
+		
+	}
+
+	@Override
+	public BlockGemOre getGemOre()
+	{
+		return ore;
+	}
+
+	@Override
+	public void setBlockItem(ItemBlock ib)
+	{
+		this.ib = ib;
+		
+	}
+
+	@Override
+	public ItemBlock getBlockItem()
+	{
+		return ib;
 	}
 }
