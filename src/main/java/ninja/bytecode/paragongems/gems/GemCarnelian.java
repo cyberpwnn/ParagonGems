cacf512656fb2ca30c0ae9ef29209ac6b929fda3
cyberpwn <danielmillst@gmail.com>
2019-08-06 18:57:15 -0400

    Textures and assets

---
+++ src/main/java/ninja/bytecode/paragongems/gems/GemCarnelian.java
@@ -0,0 +1,11 @@
+package ninja.bytecode.paragongems.gems;
+
+import ninja.bytecode.paragongems.util.Gem;
+
+public class GemCarnelian extends Gem
+{
+	public GemCarnelian()
+	{
+		super("carnelian", "Carnelean");
+	}
+}
---
+++ src/main/java/ninja/bytecode/paragongems/gems/GemSpinel.java
@@ -0,0 +1,11 @@
+package ninja.bytecode.paragongems.gems;
+
+import ninja.bytecode.paragongems.util.Gem;
+
+public class GemSpinel extends Gem
+{
+	public GemSpinel()
+	{
+		super("spinel", "Spinel");
+	}
+}
